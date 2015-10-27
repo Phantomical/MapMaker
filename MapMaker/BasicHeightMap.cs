3550fc1f30a174b9fc9e0dc1270e1a637451fd63
Phantomical <phantom@lynches.ca>
2015-10-26 23:05:37 -0400

    Added a basic height map controller

---
+++ MapMaker/BasicHeightMap.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapMaker
+{
+	public class BasicHeightMap : IHeightMap
+	{
+		public float[,] HeightArray;
+
+		public void SetPixel(int xpos, int ypos, float heightValue)
+		{
+			HeightArray[ypos, xpos] = heightValue;
+		}
+		public float GetPixel(int xpos, int ypos)
+		{
+			return HeightArray[ypos, xpos];
+		}
+
+		public int Width
+		{
+			get
+			{
+				return HeightArray.GetLength(1);
+			}
+		}
+		public int Height
+		{
+			get 
+			{
+				return HeightArray.GetLength(0);
+			}
+		}
+	}
+}
