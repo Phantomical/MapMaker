97cb54bba846c4ad65a48a2c9bf084f73c199f70
Phantomical <phantom@lynches.ca>
2015-10-29 20:38:56 -0400

    Created an adapter module for LibNoise

---
+++ MapMaker/HeightGenerators/NoiseGenerators.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibNoise;
+
+namespace MapMaker.HeightGenerators
+{
+	public class LibNoiseGenerator : IHeightMapSource
+	{
+		public IModule Module;
+
+		public float GetValue(int x, int y)
+		{
+			return (float)Module.GetValue((double)x, 0.0, (double)y);
+		}
+
+		public LibNoiseGenerator(IModule module)
+		{
+			Module = module;
+		}
+	}
+
+	public static class NoiseGenerators
+	{
+
+
+	}
+}
